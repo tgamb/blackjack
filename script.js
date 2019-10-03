ac3500a10e101467402945d2d1679e73a93a1105
Thomas Gamble <thomasgamble8170@gmail.com>
2019-10-03 10:46:02 -0400

    added createPlayers function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lines 3-21 construct the deck of cards. 
+// Function was taken from https://www.thatsoftwaredude.com
 let deck = new Array()
 let suits = ['spades', 'diamonds', 'clubs', 'hearts']
 let faceValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -18,7 +20,20 @@ function buildDeck() {
     }
 }
 
+// Lines 23-26 shuffle the deck of cards 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5)
     return deck
+}
+
+// Lines 31-39 create players that will go on the gameboard. 
+// Function was taken from https://www.thatsoftwaredude.com
+var newPlayers = new Array()
+function createPlayers(num) {
+    newPlayers = new Array() 
+    for(let i = 1; i <= num; i++) {
+        var hand = new Array() 
+        var player = {name: 'Player' + i, id: i, points: 0, hand: hand}
+        newPlayers.push(player)
+    }
 }
