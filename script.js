643d01ff23ea3ac8417ec0e3b62daae0949b4fb8
Thomas Gamble <thomasgamble8170@gmail.com>
2019-10-03 09:40:40 -0400

    wrote buildDeck function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('lets play!')
+let deck = new Array()
+let suits = ['spades', 'diamonds', 'clubs', 'hearts']
+let faceValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+function buildDeck() {
+    deck = new Array() 
+
+    for(let i = 0; i < faceValues.length; i++) {
+        for(let i = 0; i < suits.length; i++) {
+            let cardValue = parseInt(values[i])
+            if (faceValues[i] === 'J' || faceValues[i] === 'Q' || faceValues[i] === 'K')
+                cardValue = 10
+            if (faceValues[i] === 'A')
+                cardValue = 11
+            var card = {value: cardValue[i], suit: suits[i], pointValue: cardValue}
+            deck.push(card)
+        }
+    }
+}
